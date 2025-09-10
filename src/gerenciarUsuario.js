b35fdc0b59e5f9b20da434ed13cea011f4e42550
Mary Jacy <84360966+Mjss94@users.noreply.github.com>
2025-09-09 22:56:44 -0300

    Mudei a listas de nomes de usuarios para tornarem-se objetos

--- src/gerenciarUsuario.js
+++ src/gerenciarUsuario.js
@@ -1,14 +1,34 @@
-const usuarios = [ 'Mario', 'Maria', 'Joao', 'Julio', ' Ster'];
+const usuarios = [
+    {
+        nome: 'Mario',
+        email: 'Mario@hotmail.com'
+    },
+
+    {
+        nome: 'Maria',
+        email: 'Maria@hotmail.com'
+    },
+
+    {
+        nome: 'Joao',
+        email: 'Joao@yahoo.com'
+    },
+    {
+        nome: 'Julio',
+        email: 'Julio123@hotmail.com'
+    }
+];
 
 function retornarUsuarios() {
     return usuarios;
 }
 
-function adicionarNovoUsuario (nome) {
-     usuarios.push(nome);
-     
+function adicionarNovoUsuario(usuario) {
+    usuarios.push(usuario);
+
 }
 
-module.exports = { 
+module.exports = {
     retornarUsuarios,
-    adicionarNovoUsuario};
+    adicionarNovoUsuario
+};
--- test/gerenciarUsuarios.test.js
+++ test/gerenciarUsuarios.test.js
@@ -5,11 +5,22 @@ describe ( 'Testar as funcoes de gestao de usuarios', function(){
 
     it ('Validar que posso adicionar um novo nome de usuario na lista', function() {
         // 1. Adicionar um novo nome de usuarios
-        adicionarNovoUsuario('Ster');
+        adicionarNovoUsuario({
+            nome: 'Julio',
+            email: 'Julio123@hotmail.com'
+        });
+
         // 2. Retorna a lista de usuraios na caixa lista de usuarios
         const listaDeUsuarios = retornarUsuarios();
-        // 3. Comparar se o novo nome esta no fim da lista de usuarios
-        expect( listaDeUsuarios.at(-1)).to.equal('Ster');
+        
 
+        // 3. Comparar se o novo nome esta no fim da lista de usuarios
+       // expect( listaDeUsuarios.at(-1).nome).to.equal('Julio');
+       // expect( listaDeUsuarios.at(-1).email).to.equal('Julio123@hotmail.com');
+
+        expect(listaDeUsuarios.at(-1)).to.eql({
+            nome: 'Julio',
+            email: 'Julio123@hotmail.com'
+        });
     });
 })
